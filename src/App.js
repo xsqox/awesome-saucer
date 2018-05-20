1d1c97dafdb05b1eeef7566f4bf95029b9f9b3ce
Kseniya <kseniya.baluk@gmail.com>
2018-05-20 02:38:02 -0400

    Simplified win/lose logic; code cleanup

--- src/App.js
+++ src/App.js
@@ -2,28 +2,35 @@ import React, {Component} from 'react';
 import DynamicList from './List/List';
 import SaucerShip from './Saucer/saucer';
 import Message from './Message/message';
-
 import './App.css';
 
 export default class App extends Component {
     constructor() {
         super();
-        this.shuffle = this.shuffle.bind(this);
-        this.shuffleHard = this.shuffleHard.bind(this);
-        this.onSaucerClick = this.onSaucerClick.bind(this);
-        this.shuffleSaucers = this.shuffleSaucers.bind(this);
-        this.initSaucers = this.initSaucers.bind(this);
-        this.renderSaucer = this.renderSaucer.bind(this);
-        this.setRandom = this.setRandom.bind(this);
-        this.pickAnswer = this.pickAnswer.bind(this);
+        this.state = {
+            saucers: [
+                {
+                    id: 1,
+                    opened: false,
+                }, {
+                    id: 2,
+                    opened: false
+                }, {
+
+                    id: 3,
+                    opened: false
+                }
+            ],
+            playedID: null,
+            winID: this.setRandom(3),
+        };
         this.answers = {
             win: ['You won (a trip)!', 'Be my Valentine!', 'Will you marry me?', 'Warp One Engage!', 'Beam you up!', 'Come with me, into the ship', 'You found serenity!', 'Universe is yours', 'Fly over, fly over!', 'Permission to come on board!', 'Up we go!', 'Now you one of us'],
             lose: ['Nope', 'Loser!', 'No space trips for ya', 'Keep trying', 'You are pathetic', 'No luck, buddy', 'Don\'t ever play roulette', 'This is embarrassing', 'Lol', 'Crawling is your thing', 'You poor worm...', 'You are the worst', 'Haha!', 'Earthworm', 'Just one more time', 'Just go...', 'Are you done already?', 'You not gonna make it', 'Access denied', 'We don\'t need you', 'Boring', 'Lam\'oh']
         };
-        this.state = {
-            saucers: this.initSaucers(),
-            playedID: null
-        }
+        this.onSaucerClick = this.onSaucerClick.bind(this);
+        this.renderSaucer = this.renderSaucer.bind(this);
+        this.pickAnswer = this.pickAnswer.bind(this);
     }
 
     componentDidMount() {
@@ -46,59 +53,23 @@ export default class App extends Component {
         return <SaucerShip saucer={item} onClick={onClick}/>;
     }
 
-    initSaucers() {
-        let saucers = [
-            {
-                win: false,
-                id: 1,
-                opened: false,
-            }, {
-                win: false,
-                id: 2,
-                opened: false
-            }, {
-                win: false,
-                id: 3,
-                opened: false
-            }
-        ];
-        saucers = this.setRandom(saucers);
-        saucers.forEach((value, index) => {
-            value.message = this.pickAnswer(value.win ? 'win' : 'lose');
-        });
-        return saucers;
-    }
-
-    setRandom(options) {
-        const randomWinIndex = Math.floor(Math.random() * options.length);
-        options.forEach((value, index) => {
-            value.opened = false;
-            if (index === randomWinIndex) {
-                value.win = true
-            } else {
-                value.win = false;
-            }
-        });
-        return options;
+    setRandom(length) {
+        return Math.floor(Math.random() * length);
     }
 
     shuffleHard() {
-        var i = 0;
-        for (i = 0; i < 99; i++) {
+        for (let i = 0; i < 99; i++) {
             setTimeout(() => this.shuffle(), 500)
         }
     }
 
     shuffle() {
         let options = this.state.saucers.slice();
-        options = this.shuffleSaucers(this.setRandom(options));
-        options.forEach((value, index) => {
-           value.opened = false;
-           value.message = this.pickAnswer(value.win ? "win" : "lose");
-        });
+        options = this.shuffleSaucers(options);
         this.setState({
             saucers: options,
-            playedID: null
+            playedID: null,
+            winID: this.setRandom(options.length)
         });
     }
 
@@ -112,14 +83,7 @@ export default class App extends Component {
 
     onSaucerClick(id) {
         if (!this.state.playedID) {
-            let options = this.state.saucers.slice();
-            options.forEach((item) => {
-                if (item.id === id) {
-                    item.opened = true;
-                }
-            });
             this.setState({
-                saucers: options,
                 playedID: id
             });
         } else {
@@ -133,8 +97,6 @@ export default class App extends Component {
     }
 
     guessedRight(playedID) {
-        return this.state.saucers.find((value) => {
-            return value.id === playedID;
-        }).win;
+        return playedID === this.state.winID;
     }
 };
